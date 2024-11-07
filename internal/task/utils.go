4e5c59cd1d6f6fbd7beb210c57a7a27fcc85ab2e
imetheric <im.etheric@gmail.com>
2024-11-07 13:22:15 +0800

    feat: catch task pack error

--- internal/task/utils.go
+++ internal/task/utils.go
@@ -1,13 +1,33 @@
 package task
 
 import (
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/nuvosphere/nudex-voter/internal/layer2/contracts"
 	"github.com/nuvosphere/nudex-voter/internal/types"
 )
 
-func encodeCreateWalletTask(task types.CreateWalletTask) ([]byte, error) {
-	bytes := contracts.PackEvent(contracts.TaskPayloadContractMetaData, "WalletCreationRequest", uint32(V1),
-		uint32(types.TaskTypeCreateWallet), common.HexToAddress(task.User), task.Account, task.Chain, task.Index)
+func encodeCreateWalletTask(task types.CreateWalletTask) (bytes []byte, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("failed to encode task: %v", r)
+			bytes = nil
+		}
+	}()
+
+	bytes = contracts.PackEvent(
+		contracts.TaskPayloadContractMetaData,
+		"WalletCreationRequest",
+		uint32(V1),
+		uint32(types.TaskTypeCreateWallet),
+		common.HexToAddress(task.User),
+		task.Account,
+		task.Chain,
+		task.Index,
+	)
 	return bytes, nil
+}
+
+func encodeDepositTask(task types.DepositTask) ([]byte, error) {
+	return nil, nil
 }
