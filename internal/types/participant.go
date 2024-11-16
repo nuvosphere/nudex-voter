dd884586a957aaa6c193eaf5bf9b5e9a45bbacab
imetheric <im.etheric@gmail.com>
2024-11-16 16:07:03 +0800

    refactor: refactor threshold calculate

--- internal/types/participant.go
+++ internal/types/participant.go
@@ -5,19 +5,14 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-// CalculateThreshold (CalculateThreshold + 1) / length >= 2/3
+// CalculateThreshold (threshold + 1) / length >= 2/3
 func CalculateThreshold(length int) int {
 	threshold := decimal.NewFromUint64(uint64(length)).
 		Mul(decimal.NewFromInt(2)).
 		Div(decimal.NewFromInt(3)).
 		Ceil().
-		IntPart() - 1
-
-	for float64(threshold+1)/float64(length) < 2.0/3.0 {
-		threshold++
-	}
-
-	return int(threshold)
+		IntPart()
+	return int(threshold - 1)
 }
 
 type Participants []common.Address
