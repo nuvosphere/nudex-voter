ced5d62d4cdb230a99a5c0504728ba65ae45ed36
imetheric <im.etheric@gmail.com>
2024-11-16 15:17:18 +0800

    fix: fix threshold calculate bug

--- internal/types/participant.go
+++ internal/types/participant.go
@@ -5,13 +5,17 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-// CalculateThreshold  2/3.
+// CalculateThreshold (CalculateThreshold + 1) / length >= 2/3
 func CalculateThreshold(length int) int {
 	threshold := decimal.NewFromUint64(uint64(length)).
 		Mul(decimal.NewFromInt(2)).
 		Div(decimal.NewFromInt(3)).
 		Ceil().
-		IntPart()
+		IntPart() - 1
+
+	for float64(threshold+1)/float64(length) < 2.0/3.0 {
+		threshold++
+	}
 
 	return int(threshold)
 }
