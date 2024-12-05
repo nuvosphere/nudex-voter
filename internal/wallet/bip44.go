a9b1b0e7dfad40a0938dc1e71058e5b66824dc7f
Clint <clint.s@metis.io>
2024-12-05 19:51:20 +0800

    feat: add method

--- internal/wallet/bip44.go
+++ internal/wallet/bip44.go
@@ -273,3 +273,29 @@ func GenerateAddressByPath(masterPubKey *crypto.ECPoint, coinType, account uint3
 func ECPoint(key *ecdsa.PublicKey) *crypto.ECPoint {
 	return crypto.NewECPointNoCurveCheck(key.Curve, key.X, key.Y)
 }
+
+func GenerateCompressedBTCAddress(p *crypto.ECPoint) (string, error) {
+	return btcAddress(NewPublicKeyOfBtc(p).SerializeCompressed())
+}
+
+func GenerateUnCompressedBTCAddress(p *crypto.ECPoint) (string, error) {
+	return btcAddress(NewPublicKeyOfBtc(p).SerializeUncompressed())
+}
+
+func btcAddress(serializedPubKey []byte) (string, error) {
+	addr, err := btcutil.NewAddressPubKey(serializedPubKey, &chaincfg.MainNetParams)
+	if err != nil {
+		return "", fmt.Errorf("invalid public key: %w", err)
+	}
+	return addr.EncodeAddress(), nil
+}
+
+func NewPublicKeyOfBtc(p *crypto.ECPoint) *btcec.PublicKey {
+	var (
+		x = &btcec.FieldVal{}
+		y = &btcec.FieldVal{}
+	)
+	x.SetByteSlice(p.X().Bytes())
+	y.SetByteSlice(p.Y().Bytes())
+	return btcec.NewPublicKey(x, y)
+}
