833b1c71a8df81eb7766f50fc87cc05988b8467d
imetheric <im.etheric@gmail.com>
2024-12-16 19:51:24 +0800

    feat: add asset topics

--- internal/layer2/contracts/topic.go
+++ internal/layer2/contracts/topic.go
@@ -17,6 +17,9 @@ var (
 	WithdrawalRecordedTopic         = crypto.Keccak256Hash([]byte(`WithdrawalRecorded(address,uint256,uint256,bytes,bytes)`))                                                  // DepositManagerContract
 	NIP20TokenEventMintbTopic       = crypto.Keccak256Hash([]byte(`NIP20TokenEvent_mintb(address,bytes32,uint256)`))                                                           // InscriptionContract
 	NIP20TokenEventBurnbTopic       = crypto.Keccak256Hash([]byte(`NIP20TokenEvent_burnb(address,bytes32,uint256)`))                                                           // InscriptionContract
+	AssetListedTopic                = crypto.Keccak256Hash([]byte(`AssetListed(bytes32,(uint8,uint8,bool,bool,uint256,uint256,uint256,string,string))`))                       // InscriptionContract
+	AssetUpdatedTopic               = crypto.Keccak256Hash([]byte(`AssetUpdated(bytes32,(uint8,uint8,bool,bool,uint256,uint256,uint256,string,string))`))                      // InscriptionContract
+	AssetDelistedTopic              = crypto.Keccak256Hash([]byte(`AssetDelisted(bytes32)`))                                                                                   // InscriptionContract
 	WalletCreationRequestTopic      = crypto.Keccak256Hash([]byte(`WalletCreationRequest(uint8,uint8,uint32,uint8,uint8)`))                                                    // TopicPayloadContract
 	DepositRequestTopic             = crypto.Keccak256Hash([]byte(`DepositRequest(uint8,uint8,string,uint64,uint8,uint32,uint64,string,string,string,uint8,uint8)`))           // TopicPayloadContract
 	WithdrawalRequestTopic          = crypto.Keccak256Hash([]byte(`WithdrawalRequest(uint8,uint8,string,uint64,uint8,uint32,uint64,string,string,string,uint8,uint8,uint64)`)) // TopicPayloadContract
