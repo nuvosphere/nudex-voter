f502b81f8b4ad094a32f530614b103ca769a0ad1
Clint <clint.s@metis.io>
2024-12-27 23:03:55 +0800

    feat: update db

--- internal/db/operation.go
+++ internal/db/operation.go
@@ -1,17 +1,32 @@
 package db
 
 import (
-	"github.com/shopspring/decimal"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/nuvosphere/nudex-voter/internal/layer2/contracts"
 	"gorm.io/gorm"
 )
 
 type Operations struct {
 	gorm.Model
-	Nonce            decimal.Decimal `gorm:"index"`
-	Data             string
-	EvmTransactionID uint64 `gorm:"index"`
+	TssNonce  uint64 `gorm:"index"`
+	Signature []byte
+	Hash      common.Hash
+	DataHash  common.Hash
+	Data      string
 }
 
 func (*Operations) TableName() string {
 	return "operations"
+}
+
+func (o *Operations) TaskID() uint64 {
+	return o.TssNonce
+}
+
+func (o *Operations) Type() int {
+	return TaskTypeOperations
+}
+
+type TaskOperations struct {
+	Operation []contracts.TaskOperation
 }
